193681ce5db8d9d496c555321b7ec37b3df87b4e
Jonathan Feitosa <jfsjonathanfeitosa@gmail.com>
2019-12-04 13:50:10 -0400

    Innitial commit

---
+++ map.js
@@ -0,0 +1,16 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -3.0790187, lng: -60.0119109},
+    zoom: 13.4
+  });
+  createMakers();
+}
+function createMakers(){
+    var maker = new google.maps.Marker({
+        position: new google.maps.LatLng(-3.065350, -60.009441),
+        map: map,
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png"
+    })
+
+}
